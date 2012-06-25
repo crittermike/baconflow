7094723ee7d067dcc39808312dffaf1604e87fca
Mike Crittenden <mikecrittenden@gmail.com>
2012-06-25 11:18:28 -0400

    tons of crap

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var routes = function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/app.html', controller: AppCtrl});
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: LoginCtrl});
   $routeProvider.when('/account', {templateUrl: 'partials/account.html', controller: AccountCtrl});
-  $routeProvider.otherwise({redirectTo: '/app'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }
 
 // Declare app level module which depends on filters, and services
--- js/controllers.js
+++ js/controllers.js
@@ -2,11 +2,56 @@
 
 /* Controllers */
 function AppCtrl($scope) {
+  Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
+  var user = Parse.User.current();
 
+  $scope.limit = user.get("limit");
+  $scope.current = user.get("current");
+
+  $scope.addTransaction = function() {
+    $scope.current += $scope.curTransaction;
+    console.log($scope.current);
+    user.set("current", $scope.current);
+    user.save(null, {
+      success: function(user) {
+        $scope.curTransaction = 0;
+      }
+    })
+  }
 }
+
 function LoginCtrl($scope) {
 
 }
-function AccountCtrl($scope) {
 
+function AccountCtrl($scope) {
+  Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
+
+  var user = Parse.User.current();
+  if (!user) {
+    user = new Parse.User();
+  } else {
+    $scope.email = user.get("email");
+    $scope.limit = user.get("limit");
+    $scope.current = user.get("current");
+  }
+
+  $scope.saveUser = function() {
+    user.set("username", $scope.email);
+    if ($scope.password) {
+      user.set("password", $scope.password);
+    }
+    user.set("email", $scope.email);
+    user.set("limit", $scope.limit);
+    user.set("current", 0);
+
+    user.signUp(null, {
+      success: function(user) {
+        alert("You signed up! Now go track some transactions!");
+      },
+      error: function(user, error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+  }
 }
