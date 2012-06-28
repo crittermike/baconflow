f88e6d54ff598c136f8414323f2deae5319e32b7
Mike Crittenden <mikecrittenden@gmail.com>
2012-06-28 16:16:47 -0400

    WIP services

--- js/controllers.js
+++ js/controllers.js
@@ -11,47 +11,20 @@ function AppCtrl($scope) {
   $scope.addTransaction = function() {
     $scope.current += $scope.curTransaction;
     console.log($scope.current);
-    user.set("current", $scope.current);
-    user.save(null, {
-      success: function(user) {
-        $scope.curTransaction = 0;
-      }
-    })
   }
 }
 
 function LoginCtrl($scope) {
-
+  $scope.loginUser = function() {
+    var success = parseService.login($scope.email, $scope.password);
+    if (!success[0]) {
+      alert(success[1]);
+    }
+  }
 }
 
 function AccountCtrl($scope) {
-  Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
-
-  var user = Parse.User.current();
-  if (!user) {
-    user = new Parse.User();
-  } else {
-    $scope.email = user.get("email");
-    $scope.limit = user.get("limit");
-    $scope.current = user.get("current");
-  }
-
   $scope.saveUser = function() {
-    user.set("username", $scope.email);
-    if ($scope.password) {
-      user.set("password", $scope.password);
-    }
-    user.set("email", $scope.email);
-    user.set("limit", $scope.limit);
-    user.set("current", 0);
-
-    user.signUp(null, {
-      success: function(user) {
-        alert("You signed up! Now go track some transactions!");
-      },
-      error: function(user, error) {
-        alert("Error: " + error.code + " " + error.message);
-      }
-    });
+    parseService.saveUser($scope.email, $scope.password, $scope.limit);
   }
 }
--- js/services.js
+++ js/services.js
@@ -2,8 +2,44 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('baconflow.services', []).
-  value('version', '0.1');
+angular.module('baconflow.services', [])
+  .service('parseService', function () {
+    return {
+      login:function (email, password) {
+        Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
+        Parse.User.logIn(email, password, {
+          success: function(user) {
+            return [true, user];
+          },
+          error: function(user, error) {
+            return [false, "Error: " + error.code + " " + error.message];
+          }
+        });
+      },
+      saveUser:function (email, password, limit) {
+        Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
+        
+        var user = Parse.User.current();
+        if (!user) {
+          user = new Parse.User();
+        } 
+        user.set("username", $scope.email);
+        if ($scope.password) {
+          user.set("password", $scope.password);
+        }
+        user.set("email", $scope.email);
+        user.set("limit", $scope.limit);
+        user.signUp(null, {
+          success: function(user) {
+            return [true, user];
+          },
+          error: function(user, error) {
+            return [false, "Error: " + error.code + " " + error.message];
+          }
+        });
+      },
+      addTransaction:function (amount) {
+        Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
+      }
+    };
+  })
