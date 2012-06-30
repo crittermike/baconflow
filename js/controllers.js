2fdacc1152a821073f6129b1bd4b0f1161193e65
Mike Crittenden <mikecrittenden@gmail.com>
2012-06-30 16:27:56 -0400

    fix userctrl and logins

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,25 @@
 'use strict';
 
 /* Controllers */
-function MainCtrl($scope, parseService) {
+function UserCtrl($scope, parseService) {
   $scope.loggedIn = function() {
     return parseService.loggedIn();
   }
   $scope.logout = function() {
     parseService.logout();
   }
+  $scope.loginUser = function() {
+    parseService.login($scope.email, $scope.password, function(response) {
+      if (!response[0]) {
+        alert(response[1]);
+      }
+    });
+  }
+  $scope.saveUser = function() {
+    parseService.saveUser($scope.email, $scope.password, $scope.limit);
+  }
 }
+
 function AppCtrl($scope, parseService) {
   Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
   var user = parseService.loggedIn();
@@ -19,22 +30,7 @@ function AppCtrl($scope, parseService) {
     $scope.limit = 500;
     $scope.current = 0;
   }
-
-
   $scope.addTransaction = function() {
-    $scope.current += $scope.curTransaction;
-    console.log($scope.current);
-  }
-}
-
-function LoginCtrl($scope, parseService) {
-  $scope.loginUser = function() {
-    var response = parseService.login($scope.email, $scope.password);
-  }
-}
-
-function AccountCtrl($scope) {
-  $scope.saveUser = function() {
-    parseService.saveUser($scope.email, $scope.password, $scope.limit);
+    //
   }
 }
--- js/services.js
+++ js/services.js
@@ -6,17 +6,15 @@ angular.module('baconflowServices', [])
   .service('parseService', function() {
     Parse.initialize("hCnt4S3bcWaZRDUQxoz4knP8KvYncQ4UGkwqwIq1", "PrQttkfi0FHWEQwoBt3iMFX2BkqVOBpwlyS0BQB6");
     return {
-      login:function (email, password) {
-        var response;
+      login:function (email, password, callback) {
         Parse.User.logIn(email, password, {
           success: function(user) {
-            response = [true, user];
+            callback([true, user]);
           },
           error: function(user, error) {
-            response = [false, "Error: " + error.code + " " + error.message];
+            callback([false, "Error: " + error.code + " " + error.message]);
           }
         });
-        alert(response) // still undefined;
       },
 
       logout: function() {
